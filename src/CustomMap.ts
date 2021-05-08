ef6429788b2258d44d970813e1657254d55e7e1e
evilpaper <pellburt@gmail.com>
2021-05-08 18:32:01 +0200

    Add user marker to map

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -4,6 +4,9 @@ Google Map gives us so we can control what other developers can do
 with it.
 */
 
+import { User } from "./User";
+import { Company } from "./Company";
+
 export class CustomMap {
   // Adding private so nobody can change it from the outside
   private googleMap: google.maps.Map;
@@ -17,4 +20,16 @@ export class CustomMap {
       },
     });
   }
+
+  addUserMaker(user: User): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: user.location.lat,
+        lng: user.location.lng,
+      },
+    });
+  }
+
+  addCompanyMaker(company: Company): void {}
 }
